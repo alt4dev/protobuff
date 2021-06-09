e78f2840c33dceb714e1907d8243a329878f702b
Billcountry <me@billcountry.tech>
2021-06-09 17:01:29 +0300

    Build script for python

--- build.sh
+++ build.sh
@@ -12,13 +12,28 @@ function golang_build(){
     echo "Done."
 }
 
+function python_build(){
+    OUTPUT_DIR="$1"
+    echo "Building python to $OUTPUT_DIR"
+    if [ -e $OUTPUT_DIR ]; then
+        echo "Overwriting $OUTPUT_DIR"
+        rm -r $OUTPUT_DIR
+    fi
+    mkdir $OUTPUT_DIR
+    protoc --proto_path=./ --python_out=$OUTPUT_DIR definitions.proto
+    echo "Done."
+}
+
 function build_help(){
     echo "Invalid language provided. Below are the supported commands"
     echo "    ./build.sh go"
+    echo "    ./build.sh python [OUTPUT_DIR]"
 }
 
 LANGUAGE="$1"
+OUTPUT_DIR="$2"
 case $LANGUAGE in
     "go") golang_build;;
+    "python") python_build $OUTPUT_DIR;;
     *) build_help;;
 esac
